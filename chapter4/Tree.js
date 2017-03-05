73cf8fca83a0578e6bd87f3039a2fb042e5d37e6
Nick Koutrelakos <nick.koutrelakos@gmail.com>
2017-03-05 10:45:10 -0800

    adding Tree with balanced

--- chapter4/Tree.js
+++ chapter4/Tree.js
@@ -31,30 +31,71 @@ class Tree {
 		}
 	}
 
-	// left() {
-	// 	this.root = this.root.left;
-	// }
+	// check if the tree is balanced; that is, for any node, the difference of its children's heights is <= 1
 
-	// left() {
-	// 	this.root = this.root.right;
-	// }
+	balanced(root=this.root) {
+		return this._checkHeight(root) != Number.MIN_SAFE_INTEGER;
+	}
 
 	// Height is being counted by the number of 'levels' NOT by the number of edges
 
-	_getHeight(tree) {
-		if (!tree){
+	_getHeight(treeRoot){
+		if (!treeRoot){
 			return 0;
 		}
 
-		return 1 + Math.max(this._getHeight(tree.left), this._getHeight(tree.right));
+		return 1 + Math.max(this._getHeight(treeRoot.left), this._getHeight(treeRoot.right));
 	}
 
-	printTree(tree){
-		if (!tree)
-			console.log('DONE');
-		this.printTree(tree.left);
-		console.log(tree.data);
-		this.printTree(tree.right);
+	/*
+
+	~~~ HELPER function for this.balanced ~~~
+
+	-- checks height of each node's children
+	-- immediately returns Number.MIN_SAFE_INTEGER if (difference in height of a node's children) > 1
+
+	*/
+
+	_checkHeight(treeRoot){
+		if (!treeRoot)
+			return -1;
+		let leftHeight = this._checkHeight(treeRoot.left);
+		if (leftHeight === Number.MIN_SAFE_INTEGER)
+			return Number.MIN_SAFE_INTEGER
+
+		let rightHeight = this._checkHeight(treeRoot.right);
+		if (rightHeight === Number.MIN_SAFE_INTEGER)
+			return Number.MIN_SAFE_INTEGER
+
+		let heightDif = Math.abs(leftHeight - rightHeight);
+		if (heightDif > 1)
+			return Number.MIN_SAFE_INTEGER;
+		else
+			return Math.max(leftHeight, rightHeight) + 1;
+	}
+
+	printTreeInOrder(treeRoot){
+		if(treeRoot) {
+			this.printTreeInOrder(treeRoot.left);
+			console.log(treeRoot.data);
+			this.printTreeInOrder(treeRoot.right);
+		}
+	}
+
+	printTreePreOrder(treeRoot){
+		if(treeRoot) {
+			console.log(treeRoot.data);
+			this.printTreePreOrder(treeRoot.left);
+			this.printTreePreOrder(treeRoot.right);
+		}
+	}
+
+	printTreePostOrder(treeRoot){
+		if(treeRoot) {
+			this.printTreePostOrder(treeRoot.left);
+			this.printTreePostOrder(treeRoot.right);
+			console.log(treeRoot.data);
+		}
 	}
 }
 
